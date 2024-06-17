1753ae9254c42be3cc3eb143d689ba93e95314e4
tathagata <tathagatadasworkspace@gmail.com>
2024-06-17 11:04:19 +0530

    personal information card has been added, but there are some position problem for the image

---
+++ javascript/reseach-interest.js
@@ -0,0 +1,13 @@
+function createFireflies(num) {
+    for (let i = 0; i < num; i++) {
+        let firefly = document.createElement('div');
+        firefly.className = 'firefly';
+        firefly.style.left = `${Math.random() * 100}vw`;
+        firefly.style.top = `${Math.random() * 100}vh`;
+        firefly.style.animationDuration = `${Math.random() * 5 + 3}s`;
+        firefly.style.animationDelay = `${Math.random() * 5}s`;
+        document.body.appendChild(firefly);
+    }
+}
+
+createFireflies(50); // Create 50 fireflies
